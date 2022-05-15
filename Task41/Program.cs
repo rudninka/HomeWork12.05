99d4ed0cfe37dc234aba6c0fa2e6f2c36128a15f
Ekaterina <ekaterina_r21@mail.ru>
2022-05-15 16:09:09 +0300

    Сколько чисел больше нуля

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,11 +1,19 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел.
 //Посчитайте, сколько чисел больше 0 ввёл пользователь
-Console.WriteLine("Введите несколько чисел: ");
+Console.WriteLine("Введите колличество вводимых чисел: ");
 int M = int.Parse(Console.ReadLine());
 int count = 0;
-int []mass =new int[M];
-if (M > 0)
+int []mass = new int[M];
+for (int i = 0; i < M; i++)
 {
-    count++;
+    Console.WriteLine("Введите число: ");
+    mass[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < M; i++)
+{
+    if (mass[i] > 0)
+    {
+        count++;
+    }
 }
 Console.WriteLine($"Колличество чисел больше 0 - {count}");
